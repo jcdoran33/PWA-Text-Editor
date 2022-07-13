32e0b215e57d71c306be24bee9dbae76afaa7f26
Jack Doran <jcdoran33@gmail.com>
2022-07-13 15:17:39 -0400

    cleaned up database.js, removed the IF conditions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,24 +12,19 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database - Done
 export const putDb = async (content) => {
   console.log('POST request to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ todo: content });
+  const request = store.add({ value: content }); //is value correct here??
   const result = await request;
-  //add if condition to display proper message of error or success
-  if (!result) {
-    console.error('putDb not implemented - "result" is coming back undefined');
-  } else {
-    console.log('Success! Data was saved to the database. Data: ', result);
-  }
-  
+  console.log('Success! Data was saved to the database. Data: ', result);
+
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO: Add logic for a method that gets all the content from the database - Done
 export const getDb = async () => {
   console.log('GET request all from the database');
   const jateDb = await openDB('jate', 1);
@@ -37,13 +32,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  // if condition to provide proper message of error or success
-  if (!result) {
-    console.error('getDb not implemented - "result" is coming back undefined');
-  } else {
-    console.log('Success! Data was pulled from the database. Data: ', result);
-  }
-  
+  console.log('Success! Data was pulled from the database. Data: ', result);
   //then return the result
   return result;
 };
