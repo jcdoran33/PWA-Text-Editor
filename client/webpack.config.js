ec8fa12055a46137bce4618c71c6643d04711849
Jack Doran <jcdoran33@gmail.com>
2022-07-13 14:54:35 -0400

    added WebpackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,7 +29,24 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
       //new WebpackPwaManifest
-
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A simple text editor application',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'), //change assets to??...
+          },
+        ],
+      }),
     ],
 
     module: {
