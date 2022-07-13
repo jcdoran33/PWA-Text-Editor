efa6ada4ffeda06c3abb5f8d4e5ebb427ebcb2c1
Jack Doran <jcdoran33@gmail.com>
2022-07-13 15:09:19 -0400

    added get Db method contents and if condition

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('POST to the database');
+  console.log('POST request to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -31,7 +31,21 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  console.log('GET request all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  // if condition to provide proper message of error or success
+  if (!result) {
+    console.error('getDb not implemented');
+  } else {
+    console.log('Success! Data was pulled from the database. Data: ', result);
+  }
+  
+  //then return the result
+  return result;
 };
 
 initdb();
