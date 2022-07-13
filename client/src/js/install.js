3b398384bd0c985d4011c30ac156520e3f70546d
Jack Doran <jcdoran33@gmail.com>
2022-07-13 15:23:25 -0400

    added second event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,7 +10,19 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    //if theres not prompt, end
+    if (!promptEvent) {
+        return;
+    }
+    //display prompt to user
+    promptEvent.prompt();
+    //reset window.deferredPrompt to null
+    window.deferredPrompt = null;
+    //re-add the hidden class to the install button element
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
